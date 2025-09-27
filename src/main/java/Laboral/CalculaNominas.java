595ed5e48697d3de51a158c27dd0787be3df2229
Iris <acasper631@g.educaand.es>
2025-09-27 21:04:37 +0200

    Menús completados, métodos extraídos, falta organizar los métodos y el main

--- src/main/java/Laboral/CalculaNominas.java
+++ src/main/java/Laboral/CalculaNominas.java
@@ -1,15 +1,13 @@
 package Laboral;
 
 import java.io.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 // Funcionalidad principal de la aplicación, donde probaremos los diferentes módulos.
+
 public class CalculaNominas {
     static void main(String[] args) throws IOException, SQLException {
 
@@ -23,8 +21,9 @@ public class CalculaNominas {
 
         lecturaTxt(empleadosTxt, empleadosMap);
 
+        Connection connection = null;
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
+            connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
 
             for (Empleado each : empleadosMap.values()) {
                 escribe(each);
@@ -58,15 +57,7 @@ public class CalculaNominas {
                 statement.executeUpdate();
             }
 
-            try (PreparedStatement statement = connection.prepareStatement("""
-                      UPDATE nominas SET sueldo = ? WHERE dni = ?
-                    """)) {
-                for (Empleado each : empleadosMap.values()) {
-                    statement.setInt(1, Nomina.sueldo(each));
-                    statement.setString(2, each.dni);
-                    statement.executeUpdate();
-                }
-            }
+            actualizarSueldosCompleto(connection, empleadosMap);
 
         } catch (DatosNoCorrectosException e) {
             System.out.println(e.getMessage());
@@ -75,6 +66,26 @@ public class CalculaNominas {
 
         guardarTxt(empleadosTxt, empleadosMap);
         guardarBat(sueldosBat, empleadosMap);
+
+        for (Empleado each : empleadosMap.values()) {
+            escribe(each);
+        }
+        menu(empleadosMap);
+        if (connection != null) {
+            connection.close();
+        }
+    }
+
+    private static void actualizarSueldosCompleto(Connection connection, Map<String, Empleado> empleadosMap) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement("""
+                  UPDATE nominas SET sueldo = ? WHERE dni = ?
+                """)) {
+            for (Empleado each : empleadosMap.values()) {
+                statement.setInt(1, Nomina.sueldo(each));
+                statement.setString(2, each.dni);
+                statement.executeUpdate();
+            }
+        }
     }
 
     private static void guardarTxt(String empleadosTxt, Map<String, Empleado> empleadosMap) {
@@ -198,40 +209,220 @@ public class CalculaNominas {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
             lecturaTxt(empleadosNuevosTxt, empleadosMap);
-                try (PreparedStatement statement = connection.prepareStatement("""
-                          INSERT INTO empleados VALUES (?, ?, ?, ?, ?)
-                          ON DUPLICATE KEY UPDATE nombre=?, sexo=?, anyos=?, categoria=?;
+            try (PreparedStatement statement = connection.prepareStatement("""
+                      INSERT INTO empleados VALUES (?, ?, ?, ?, ?)
+                      ON DUPLICATE KEY UPDATE nombre=?, sexo=?, anyos=?, categoria=?;
+                    """)) {
+                for (Empleado each : empleadosMap.values()) {
+                    statement.setString(1, each.dni);
+                    statement.setString(2, each.nombre);
+                    statement.setString(3, String.valueOf(each.sexo));
+                    statement.setInt(4, each.anyos);
+                    statement.setInt(5, each.getCategoria());
+                    statement.setString(6, each.nombre);
+                    statement.setString(7, String.valueOf(each.sexo));
+                    statement.setInt(8, each.anyos);
+                    statement.setInt(9, each.getCategoria());
+                    statement.addBatch();
+                }
+                statement.executeBatch();
+
+                try (PreparedStatement statement2 = connection.prepareStatement("""
+                          INSERT INTO nominas VALUES (?, ?)
+                          ON DUPLICATE KEY UPDATE sueldo=?
                         """)) {
                     for (Empleado each : empleadosMap.values()) {
-                        statement.setString(1, each.dni);
-                        statement.setString(2, each.nombre);
-                        statement.setString(3, String.valueOf(each.sexo));
-                        statement.setInt(4, each.anyos);
-                        statement.setInt(5, each.getCategoria());
-                        statement.setString(6, each.nombre);
-                        statement.setString(7, String.valueOf(each.sexo));
-                        statement.setInt(8, each.anyos);
-                        statement.setInt(9, each.getCategoria());
-                        statement.addBatch();
-                    }
-                    statement.executeBatch();
-
-                    try (PreparedStatement statement2 = connection.prepareStatement("""
-                              INSERT INTO nominas VALUES (?, ?)
-                              ON DUPLICATE KEY UPDATE sueldo=?
-                            """)) {
-                        for (Empleado each : empleadosMap.values()) {
                         statement2.setString(1, each.dni);
                         statement2.setInt(2, Nomina.sueldo(empleadosMap.get(each.dni)));
                         statement2.setInt(3, Nomina.sueldo(empleadosMap.get(each.dni)));
                         statement2.addBatch();
                     }
-                        statement2.executeBatch();
+                    statement2.executeBatch();
                 }
             }
 
         } catch (IOException | SQLException e) {
             throw new RuntimeException(e);
+        }
+    }
+
+    private static void menu(Map<String, Empleado> empleados) throws SQLException {
+        Scanner sca = new Scanner(System.in);
+        int opcion = -1;
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
+
+        while (opcion != 0) {
+            System.out.print("""
+                    MENU EMPLEADOS
+                    ---------------------------------
+                    1-Mostrar todos los datos
+                    2-Buscar salario por DNI
+                    3-Modificar datos empleados
+                    4-Actualizar nómina empleado por DNI
+                    5-Actualizar todas las nóminas
+                    6-Copia de seguridad en ficheros
+                    0-Salir
+                    --------------------------------
+                    Elige la opción:
+                    """);
+            opcion = sca.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    mostrarEmpleadosBD();
+                    break;
+                case 2:
+                    mostrarEmpleadoPorDNI();
+                    break;
+                case 3:
+                    menuModificacionBD(empleados);
+                    break;
+                case 4:
+                    Scanner scan = new Scanner(System.in);
+                    System.out.println("Introduce el DNI del usuario a consultar");
+                    String dni = scan.nextLine();
+                    actualizarSueldo(empleados, connection, dni);
+                    break;
+                case 5:
+                    actualizarSueldosCompleto(connection, empleados);
+                    break;
+                case 6:
+                    System.out.println("Realizando copia en empleados.txt...");
+                    guardarTxt("res/empleados.txt", empleados);
+                    System.out.println("Realizando copia en sueldos.bat...");
+                    guardarBat("res/sueldos.bat", empleados);
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+                    break;
+            }
+        }
+        System.out.println("Adiós muy buenas");
+    }
+
+    private static void menuModificacionBD(Map<String, Empleado> empleados) throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Introduce el DNI cuyos datos quieres modificar: ");
+        String dni = scan.nextLine();
+        int opcion2;
+        do {
+            System.out.printf("""
+                    MENU MODIFICACIÓN DE DATOS DEL USUARIO CON DNI %s
+                    ---------------------------------
+                    1-Modificar nombre
+                    2-Modificar sexo
+                    3-Modificar años de experiencia
+                    4-Modificar categoría
+                    5-Seleccionar otro DNI
+                    0-Salir
+                    ---------------------------------
+                    Elegir la opción:
+                    """, dni);
+            opcion2 = scan.nextInt();
+            Scanner sc = new Scanner(System.in);
+            PreparedStatement stmt;
+
+            switch (opcion2) {
+                case 1:
+                    System.out.println("Nuevo nombre: ");
+                    String nombre = sc.nextLine();
+                    stmt = conn.prepareStatement("UPDATE empleados SET nombre = ? WHERE dni = ?");
+                    stmt.setString(1, nombre);
+                    stmt.setString(2, dni);
+                    stmt.executeUpdate();
+                    empleados.get(dni).nombre = nombre;
+                    break;
+                case 2:
+                    System.out.println("Nuevo sexo: ");
+                    String sexo = sc.nextLine();
+                    stmt = conn.prepareStatement("UPDATE empleados SET sexo = ? WHERE dni = ?");
+                    stmt.setString(1, sexo);
+                    stmt.setString(2, dni);
+                    stmt.executeUpdate();
+                    empleados.get(dni).sexo = sexo.charAt(0);
+                    break;
+                case 3:
+                    System.out.println("Nueva antigüedad: ");
+                    int anyos = scan.nextInt();
+                    stmt = conn.prepareStatement("UPDATE empleados SET anyos = ? WHERE dni = ?");
+                    stmt.setInt(1, anyos);
+                    stmt.setString(2, dni);
+                    stmt.executeUpdate();
+                    empleados.get(dni).anyos = anyos;
+                    actualizarSueldo(empleados, conn, dni);
+                    break;
+                case 4:
+                    System.out.println("Nueva categoría: ");
+                    int categoria = scan.nextInt();
+                    stmt = conn.prepareStatement("UPDATE empleados SET categoria = ? WHERE dni = ?");
+                    stmt.setInt(1, categoria);
+                    stmt.setString(2, dni);
+                    stmt.executeUpdate();
+                    empleados.get(dni).setCategoria(categoria);
+                    actualizarSueldo(empleados, conn, dni);
+                    break;
+                case 5:
+                    System.out.println("Introduce el DNI cuyos datos quieres modificar: ");
+                    dni = sc.nextLine();
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        } while (opcion2 != 0);
+    }
+
+    private static void actualizarSueldo(Map<String, Empleado> empleados, Connection conn, String dni) throws SQLException {
+        PreparedStatement statement2 = conn.prepareStatement("""
+                  INSERT INTO nominas VALUES (?, ?)
+                """);
+        statement2.setString(1, dni);
+        statement2.setInt(2, Nomina.sueldo(empleados.get(dni)));
+        statement2.executeUpdate();
+    }
+
+    private static void mostrarEmpleadoPorDNI() throws SQLException {
+        System.out.println("Selecciona el DNI a consultar: ");
+        Scanner sc = new Scanner(System.in);
+        String dni = sc.nextLine();
+        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
+        try {
+            PreparedStatement statement = conn.prepareStatement("""
+                    SELECT * FROM empleados WHERE dni = ?
+                    """);
+            statement.setString(1, dni);
+            ResultSet rs = statement.executeQuery();
+            rs.next(); // Empieza contando por 0, que es un objeto vacío, por eso lo saltamos
+            System.out.println("DNI: " + rs.getString(1));
+            System.out.println("Nombre: " + rs.getString(2));
+            System.out.println("Sexo: " + rs.getString(3));
+            System.out.println("Años: " + rs.getInt(4));
+            System.out.println("Categoría: " + rs.getInt(5) + "\n");
+        } catch (SQLException e) {
+            System.err.print("Error de SQL: " + e.getMessage());
+        }
+    }
+
+    private static void mostrarEmpleadosBD() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/gestion_nominas", "root", "123456");
+        try {
+            PreparedStatement statement = conn.prepareStatement("""
+                    SELECT * FROM empleados
+                    """);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                System.out.println("DNI: " + rs.getString(1));
+                System.out.println("Nombre: " + rs.getString(2));
+                System.out.println("Sexo: " + rs.getString(3));
+                System.out.println("Años: " + rs.getInt(4));
+                System.out.println("Categoría: " + rs.getInt(5) + "\n");
+            }
+        } catch (SQLException e) {
+            System.err.print("Error de SQL: " + e.getMessage());
         }
     }
 }
--- src/main/java/Laboral/Nomina.java
+++ src/main/java/Laboral/Nomina.java
@@ -4,7 +4,7 @@ package Laboral;
  * Clase de utilidad para calcular el sueldo final de los empleaods.
  */
 public class Nomina {
-    private static final int SUELDO_BASE[] = {50000, 70000, 90000, 110000, 130000,
+    private static final int[] SUELDO_BASE = {50000, 70000, 90000, 110000, 130000,
             150000, 170000, 190000, 210000, 230000};
 
     /**
